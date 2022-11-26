eb243289d44a0b67ec113b614df059ae29e4d03d
Vincent Terpstra <vdterpst@gmail.com>
2022-11-26 18:50:06 -0500

    using => operators

--- C_Core_Fundamentals/RailRoadProgramming/Result.cs
+++ C_Core_Fundamentals/RailRoadProgramming/Result.cs
@@ -1,23 +1,23 @@
-﻿using Xunit.Sdk;
+﻿using System.ComponentModel.Design.Serialization;
+using Xunit.Sdk;
 
 namespace RangeExtensions.RailRoadProgramming;
 
 public class Result<T>
 {
     public T Value { get; }
-
-    public bool IsFailure => !IsSuccess;
-    public bool IsSuccess { get; }
-    
     public Error Error { get; }
+    public bool IsSuccess { get; }
+    public bool IsFailure => !IsSuccess;
     
-    internal Result(T value)
+    private Result(T value)
     {
         Value = value;
         IsSuccess = true;
+        Error = Error.None;
     }
 
-    internal Result(Error error)
+    private Result(Error error)
     {
         Error = error;
         IsSuccess = false;
@@ -27,13 +27,11 @@ public class Result<T>
                   : new Result<TOutput>( mappingFunction(Value) );
 
     public Result<T> Ensure(Func<T, bool> ensureCheck, Error error)
-    {
-        return IsFailure || ensureCheck(Value) ? this :
-            new Result<T>(error);
-    }
+        => IsFailure || ensureCheck(Value) 
+            ? this : new Result<T>(error);
     
-    public static Result<TInput> Create<TInput>(TInput value)
-    {
-        return value is null ? new Result<TInput>(Error.NullValue) : new Result<TInput>(value);
-    }
+    
+    public static Result<TInput> Create<TInput>(TInput value) =>
+        value is null ? new Result<TInput>(Error.NullValue) : new Result<TInput>(value);
+    
 }
