80139ba00df48fe40f57f828015a83dfd3583713
Vincent Terpstra <vdterpst@gmail.com>
2022-11-30 17:02:22 -0500

    Fun with Implicit and Explicit casting

---
+++ C_Core_Fundamentals/TypeConversion/ImplicitMain.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace RangeExtensions.TypeConversion;
+
+public class ImplicitMain
+{
+    [Fact]
+    public void TestingImplicitOperator()
+    {
+        Guid id = Guid.NewGuid();
+
+        //Constructor
+        //UserId newUser = new UserId(id);
+        //implicit assignment
+        UserId user = id;
+
+        //Explicit operator forces casting to Guid
+        Guid userGuid = (Guid)user;
+
+    }
+}
---
+++ C_Core_Fundamentals/TypeConversion/UserId.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+
+namespace RangeExtensions.TypeConversion;
+
+public class UserId
+{
+    public Guid Value { get; }
+
+    public UserId(Guid value)
+    {
+        Value = value;
+    }
+
+    
+    //Allows conversion of Guid to UserId
+    public static implicit operator UserId(Guid guid)
+    {
+        return new UserId(guid);
+    }
+    
+    //Implicit conversion back
+    // public static implicit operator Guid(UserId userId)
+    // {
+    //     return userId.Value;
+    // }
+    
+    public static explicit operator Guid(UserId userId)
+    {
+        return userId.Value;
+    }
+    
+    
+}
