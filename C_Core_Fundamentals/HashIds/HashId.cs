80c3fd60b34f73b5731ce7c15dbbe1f79e1df05d
Vincent Terpstra <vdterpst@gmail.com>
2022-12-14 16:45:55 -0500

    Adding Hash Ids library and tests
    Hash Id's allow hashing and returning sequential 'guids'

---
+++ C_Core_Fundamentals/HashIds/HashId.cs
@@ -0,0 +1,24 @@
+﻿using HashidsNet;
+
+namespace RangeExtensions.HashIds;
+
+public class HashId
+{
+    private readonly IHashids _hashids;
+
+    public HashId(IHashids hashids)
+    {
+        _hashids = new Hashids("Salty", minHashLength: 11);
+    }
+
+    public string GetHashId(int number)
+    {
+        return _hashids.Encode(number);
+    }
+
+    public int DecodeHashId(string hash)
+    {
+        return _hashids.Decode(hash)[0];
+    }
+    
+}
---
+++ C_Core_Fundamentals/HashIds/HashIdTest.cs
@@ -0,0 +1,65 @@
+﻿using System.ComponentModel.DataAnnotations;
+using HashidsNet;
+using Xunit;
+
+namespace RangeExtensions.HashIds;
+
+public class HashIdTest
+{
+    private readonly HashId _hashids;
+
+    public HashIdTest()
+    {
+        _hashids = new HashId(new Hashids("random salt", minHashLength: 11));
+    }
+
+    [Theory]
+    [InlineData(100)]
+    [InlineData(101)]
+    public void HashingIdsIsReversable(int input)
+    {
+        //Arrange
+        //Act
+        string hash = _hashids.GetHashId(input);
+        int decode = _hashids.DecodeHashId(hash);
+        
+        //Assert
+        Assert.Equal(input, decode);
+    }
+
+    [Theory]
+    [InlineData(100)]
+    [InlineData(101)]
+    public void HashingIdsProducesSameResult(int input)
+    {
+        //Arrange
+
+        //Act
+        string firstHash = _hashids.GetHashId(input);
+        string secondHash = _hashids.GetHashId(input);
+        string thirdHash = _hashids.GetHashId(input);
+        
+        //Assert
+        Assert.Equal(firstHash, secondHash);
+        Assert.Equal(secondHash, thirdHash);
+    }
+
+    [Fact]
+    public void TwoRandomHashesAreNotIdentical()
+    {
+        //Arrange
+        Random rnd = new Random();
+
+        int first = rnd.Next();
+        int second = rnd.Next();
+        if (first == second)
+            first++;
+
+        //Act
+        string firstHash = _hashids.GetHashId(first);
+        string secondHash = _hashids.GetHashId(second);
+
+        //Assert
+        Assert.NotEqual(firstHash, secondHash);
+    }
+}
