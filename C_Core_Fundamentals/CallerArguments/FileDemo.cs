23cd7e5034f2cf0985493c16d1d6b7d0fdd3b9d0
Vincent Terpstra <vdterpst@gmail.com>
2023-01-21 20:01:43 -0500

    Playing around with CallerArguments

---
+++ C_Core_Fundamentals/CallerArguments/FileDemo.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace C_Core_Fundamentals.CallerArguments;
+
+public class FileDemo
+{
+    public static void Example([CallerFilePath]string path = default!, 
+        [CallerLineNumber]int number = 0
+    )
+    {
+        Console.WriteLine(path);
+        Console.WriteLine(number);
+    }
+
+    public static void Test()
+    {
+        Example();
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using C_Core_Fundamentals.CallerArguments;
+
+Console.WriteLine("Hello, World!");
+ArgExpressionDemo.Test();
+FileDemo.Test();
+FileDemo.Example();
