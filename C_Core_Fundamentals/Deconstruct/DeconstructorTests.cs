a3e3a01a38b039db36624dec912107b71e756e50
Vincent Terpstra <vdterpst@gmail.com>
2023-03-08 18:29:15 -0500

    fun with deconstruct functions

---
+++ C_Core_Fundamentals/Deconstruct/DeconstructorTests.cs
@@ -0,0 +1,26 @@
+﻿namespace C_Core_Fundamentals.Deconstruct;
+
+public class DeconstructTests
+{
+    [Fact]
+    public void DeconstructTestShouldReturnValues()
+    {
+        TestClass test = new()
+        {
+            StringValue = "Hello world",
+            IntValue = 42,
+            DateValue = new DateOnly(1991, 01, 01)
+        };
+
+        var (value, _, date) = test;
+
+        var (day, month, year) = date;
+        
+        Assert.Equal(date, test.DateValue);
+        Assert.Equal(value, test.IntValue);
+        
+        Assert.Equal(day, test.DateValue.Day);
+        Assert.Equal(year, test.DateValue.Year);
+        Assert.Equal(month, test.DateValue.Month);
+    }
+}
---
+++ C_Core_Fundamentals/Deconstruct/Deconstructors.cs
@@ -0,0 +1,26 @@
+﻿namespace C_Core_Fundamentals.Deconstruct;
+#nullable disable
+
+public class TestClass
+{
+    public int IntValue { get; init; }
+    public string StringValue { get; init; }
+    
+    public DateOnly DateValue { get; init; }
+    public void Deconstruct(out int value, out string str, out DateOnly date)
+    {
+        value = IntValue;
+        str = StringValue;
+        date = DateValue;
+    }
+}
+
+public static class DeconstructExtensions
+{
+    public static void Deconstruct(this DateOnly date, out int day, out int month, out int year)
+    {
+        year = date.Year;
+        month = date.Month;
+        day = date.Day;
+    }
+}
